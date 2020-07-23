bc87181a0a4cdd154e322aceddadda62c62712b3
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-07-23 02:15:14 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :authors, only: [:show]
 
-  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+    resources :authors, only: [:show] do
+      # nested resource for posts
+      resources :posts, only: [:show, :index]
+    end
 
-  root 'posts#index'
+    resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
+    root 'posts#index'
 end
