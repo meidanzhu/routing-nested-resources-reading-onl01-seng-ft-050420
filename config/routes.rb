99ad59e1213ece5cfc21e48986629cf6101b48c3
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-07-23 02:11:46 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :authors, only: [:show]
+  get 'authors/:id/posts', to: 'authors#posts_index'
+  get 'authors/:id/posts/:post_id', to: 'authors#post'
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
