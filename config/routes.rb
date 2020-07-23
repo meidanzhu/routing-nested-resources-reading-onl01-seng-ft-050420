f36d8fbdc1ca3b1be18afb92c8572fb9ad9f3a6d
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-07-23 02:13:05 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :authors, only: [:show]
-  get 'authors/:id/posts', to: 'authors#posts_index'
-  get 'authors/:id/posts/:post_id', to: 'authors#post'
 
-  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+    resources :authors, only: [:show] do
+      # nested resource for posts
+      resources :posts, only: [:show, :index]
+    end
 
-  root 'posts#index'
+    resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
+    root 'posts#index'
 end
